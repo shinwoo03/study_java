f30bb07d0eae1c89b5b9009d4ffdea149adbde01
shinwoo03 <tkfkd03032@gmail.com>
2024-05-03 17:38:13 +0900

    숙제

---
+++ _01.Variable/src/homework01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class homework01 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner( System.in );
+	
+	System.out.println("당신의 나이는?");
+	
+	
+}
+}
